19ca08a1da93250905e82c9c71729243f327155e
Christian Roberto <christian2020022014@gmail.com>
2023-12-11 13:03:07 -0400

    Criação de filtro Search pelo nome do lanche

--- Components/CarrinhoCompraResumo.cs
+++ Components/CarrinhoCompraResumo.cs
@@ -15,15 +15,15 @@ namespace LanchesMac.Components
 
         public IViewComponentResult Invoke()
         {
-            //var itens = _carrinhoCompra.GetCarrinhoCompraItems();
-            //_carrinhoCompra.CarrinhoCompraItems = itens;    
+            var itens = _carrinhoCompra.GetCarrinhoCompraItems();
+            _carrinhoCompra.CarrinhoCompraItems = itens;
 
-            var itens = new List<CarrinhoCompraItem>()
-            {
-                new CarrinhoCompraItem(),
-                new CarrinhoCompraItem()
-            };
-            _carrinhoCompra.CarrinhoCompraItems = itens;    
+            //var itens = new List<CarrinhoCompraItem>()
+            //{
+            //    new CarrinhoCompraItem(),
+            //    new CarrinhoCompraItem()
+            //};
+            //_carrinhoCompra.CarrinhoCompraItems = itens;    
 
             var carrinhoCompraVM = new CarrinhoCompraViewModel
             {
